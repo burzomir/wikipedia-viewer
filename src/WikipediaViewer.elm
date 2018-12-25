9f7a1eee114066f5d11cedc771de9ea38f10b3d2
burzomir <burzomirdev@gmail.com>
2018-12-26 00:12:18 +0100

    Create searchBox

--- src/WikipediaViewer.elm
+++ src/WikipediaViewer.elm
@@ -1,4 +1,56 @@
-import Html exposing (h1, text)
+import Browser
+import Html exposing (Html, div, text, form, input, button)
+import Html.Events exposing (onSubmit, onInput)
+import Html.Attributes exposing (style)
 
 main =
-  h1 [] [ text "Wikiepedia Viewer"]
+  Browser.sandbox { init = init, update = update, view = view }
+
+
+type alias Model =
+  { searchValue: String
+  , searching: Bool
+  }
+
+
+init : Model
+init = 
+  { searchValue = ""
+  , searching = False
+  }
+
+
+type Msg 
+  = ChangeSearchValue String
+  | Search
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+
+    ChangeSearchValue value -> 
+      { model | searchValue = value}
+
+    Search ->
+      { model | searching = True}
+
+
+view : Model -> Html Msg
+view model =
+  div []
+  [ searchBox model.searchValue 
+  , div [] [ text (if model.searching then "Searching" else "") ]
+  ]
+
+
+searchBox: String -> Html Msg
+searchBox value =
+  form 
+    [ onSubmit Search
+    , style "textAlign" "center" 
+    ]
+    [ input [ onInput ChangeSearchValue ] [ text value ]
+    , button [] [ text "Search" ]
+    ]
+  
